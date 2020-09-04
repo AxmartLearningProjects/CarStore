3e9c69d272e293bf899252fc8d712397fc42f671
Axmart <notaxmart@gmail.com>
2020-09-04 22:55:27 +0200

    Console interface layout

--- CarShopConsoleApp/Program.cs
+++ CarShopConsoleApp/Program.cs
@@ -12,17 +12,28 @@ namespace CarShopConsoleApp
         static void Main(string[] args)
         {
 
-            Car c = new Car();
-            Car d = new Car("Ford", "Mustang", 9200);
-
-            Console.WriteLine($"Car c is as follows : {c.Make}\t{c.Model}\t{c.Price}");
-            Console.WriteLine($"Car c is as follows : {d.Make}\t{d.Model}\t{d.Price}");
+            Console.WriteLine("Welcome to the car store. First You must create some car inventory. Then you may add some cars to the shopping cart. Finally you may checkout wich will give you a total value of the shopping cart.");
 
 
-            
+            int action = chooseAction();
+            while (action != 0)
+            {
+                Console.WriteLine($"You chose {action}");
+               action = chooseAction();
+            }
+
+   
+
+        }
+
+        public static int chooseAction()
+        {
+            int choice = 0;
+            Console.WriteLine("Choose an action (0) to quit (1) to add a new car to inventory (2) add car to cart (3) checkout");
+            choice = int.Parse(Console.ReadLine());
+            return choice;
 
 
-            Console.ReadKey();
 
         }
     }
