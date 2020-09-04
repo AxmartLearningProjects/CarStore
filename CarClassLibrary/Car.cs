b948cfa214cc4a5ccf62604e453b426ea826b053
Axmart <notaxmart@gmail.com>
2020-09-04 22:16:10 +0200

    Add project files.

---
+++ CarClassLibrary/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarClassLibrary
+{
+    public class Car
+    {
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        public Car()
+        {
+
+            Make = "nothing yet";
+            Model = "nothing yet";
+            Price = 0.00M;
+
+        }
+
+        public Car(string makeName, string modelName, int priceAmount)
+        {
+            Make = makeName;
+            Model = modelName;
+            Price = priceAmount;
+
+
+        }
+
+
+
+    }
+}
---
+++ CarShopConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using CarClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShopConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car c = new Car();
+            Car d = new Car("Ford", "Mustang", 9200);
+
+            Console.WriteLine($"Car c is as follows : {c.Make}\t{c.Model}\t{c.Price}");
+            Console.WriteLine($"Car c is as follows : {d.Make}\t{d.Model}\t{d.Price}");
+
+
+            
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
