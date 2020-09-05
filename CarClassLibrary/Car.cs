8152f7dc8f3da3954890ab6356eae9e01bb21658
Axmart <notaxmart@gmail.com>
2020-09-05 17:59:18 +0200

    Checkout and error handling

--- CarClassLibrary/Car.cs
+++ CarClassLibrary/Car.cs
@@ -10,6 +10,9 @@ namespace CarClassLibrary
         public string Make { get; set; }
         public string Model { get; set; }
         public decimal Price { get; set; }
+        public string Color { get; set; }
+        public bool Is4Wheeler { get; set; }
+
 
         public Car()
         {
@@ -17,21 +20,23 @@ namespace CarClassLibrary
             Make = "nothing yet";
             Model = "nothing yet";
             Price = 0.00M;
-
+            Color = "nothing yet";
+            Is4Wheeler = false;
         }
 
-        public Car(string makeName, string modelName, decimal priceAmount)
+        public Car(string makeName, string modelName, decimal priceAmount, string colorName, bool is4WheelerCar)
         {
             Make = makeName;
             Model = modelName;
             Price = priceAmount;
-
+            Color = colorName;
+            Is4Wheeler = is4WheelerCar;
 
         }
 
         override public string ToString()
         {
-            return $"Make : {Make} Model : {Model} Price :{Price}";
+            return $"Make : {Make} Model : {Model} Price :{Price} Color: {Color} 4 wheeler: {Is4Wheeler}";
 
         }
 
--- CarShopConsoleApp/Program.cs
+++ CarShopConsoleApp/Program.cs
@@ -24,27 +24,105 @@ namespace CarShopConsoleApp
 
                 switch (action)
                 {
+
+                    // Add item to inventory
                     case 1:
                         Console.WriteLine("You choose to add a new car to the inventory");
 
                         string carMake = string.Empty;
                         string carModel = string.Empty;
                         decimal carPrice = 0.00M;
+                        string carColor = string.Empty;
+                        bool is4wheeler = false;
 
                         Console.WriteLine("What is the car make?");
                         carMake = Console.ReadLine();
                         Console.WriteLine("What is the car model?");
                         carModel = Console.ReadLine();
                         Console.WriteLine("What is the car price?");
-                        carPrice = decimal.Parse(Console.ReadLine());
+                        try
+                        {
+                            carPrice = decimal.Parse(Console.ReadLine());
+                        }
+                        catch (Exception) 
+                        {
+                            Console.WriteLine("Please enter a valid number");
+                            while (!decimal.TryParse(Console.ReadLine(),out carPrice))
+                            {
+                                Console.WriteLine("Please enter a valid number");
+                            }
 
-                        Car newCar = new Car(carMake, carModel, carPrice);
+                        }
+                        
+                        Console.WriteLine("What is the color?");
+                        carColor = Console.ReadLine();
+                        Console.WriteLine("The car is a 4 wheeler? ");
+                        try
+                        {
+                            is4wheeler = bool.Parse(Console.ReadLine());
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Please type true or false");
+                            while (!bool.TryParse(Console.ReadLine(),out is4wheeler))
+                            {
+                                Console.WriteLine("Please type true or false");
+                            }
+                            
+                        }
+
+
+                        Car newCar = new Car(carMake, carModel, carPrice, carColor,is4wheeler);
                         s.CarList.Add(newCar);
 
                         printInventory(s);
 
                         break;
 
+                    // Add to cart
+                    case 2:
+                        Console.WriteLine("You  choose to add a car to your shopping cart");
+                        printInventory(s);
+                        Console.WriteLine("Wich item woulkd you like to buy? (number)");
+                        int carChosen;
+                        try
+                        {
+                            carChosen = int.Parse(Console.ReadLine());
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Please enter a valid number");
+                            while (int.TryParse(Console.ReadLine(),out carChosen))
+                            {
+                                Console.WriteLine("Please enter a valid number");
+                            }
+                            
+                        }
+
+
+                        try
+                        {
+                            s.ShoppingList.Add(s.CarList[carChosen]);
+                        }
+                        catch (Exception)
+                        {
+                            
+
+                                Console.WriteLine("There is no car for this number");
+
+                        }
+                       
+
+                        printShoppingCart(s);
+                        break;
+
+
+                    case 3:
+
+                        printShoppingCart(s);
+                        Console.WriteLine($"The total cost of your items is :  {s.Checkout()}");
+
+                        break;
 
                     default:
                         break;
@@ -57,19 +135,46 @@ namespace CarShopConsoleApp
 
         }
 
+        private static void printShoppingCart(Store s)
+        {
+
+            Console.WriteLine("Cars you have chosen to buy.");
+            for (int i = 0; i < s.ShoppingList.Count; i++)
+            {
+                Console.WriteLine($"Car # {i} : {s.ShoppingList[i]}");
+            }
+        }
+
         private static void printInventory(Store s)
         {
-            foreach (var c in s.CarList)
+            for (int i = 0; i < s.CarList.Count; i++)
             {
-                Console.WriteLine($"Car : {c}");
-            }
+                Console.WriteLine($"Car # {i} : {s.CarList[i]}");
+            } 
+
         }
 
         public static int chooseAction()
         {
             int choice = 0;
             Console.WriteLine("Choose an action (0) to quit (1) to add a new car to inventory (2) add car to cart (3) checkout");
-            choice = int.Parse(Console.ReadLine());
+            try
+            {
+                choice = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                
+                Console.WriteLine("Please type a valid number between 0 and 3");
+                while (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Please type a valid number");
+
+
+                }
+
+            }
+
             return choice;
 
 
